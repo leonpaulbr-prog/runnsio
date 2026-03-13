b6201a1fe443028f54584ffb24cb9e2fb2695318
leonpaulbr-prog <leonpaulbr@gmail.com>
2026-03-13 14:56:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -230,3 +230,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
